9da6a10f74164ab66d38b8f6dd28d45a6d7869e8
Chris Patterson <chris@phatboyg.com>
2012-01-17 20:38:47 -0600

    Adjusted network id test to have an assertion

--- src/NewId.Tests/NetworkAddress_Specs.cs
+++ src/NewId.Tests/NetworkAddress_Specs.cs
@@ -1,8 +1,7 @@
 ﻿namespace NewId.Tests
 {
-    using System;
-    using System.Net.NetworkInformation;
     using NUnit.Framework;
+    using Providers;
 
     [TestFixture]
     public class When_getting_a_network_address_for_the_id_generator
@@ -10,23 +9,12 @@
         [Test]
         public void Should_pull_the_network_adapter_mac_address()
         {
-            NetworkInterface[] adapters = NetworkInterface.GetAllNetworkInterfaces();
-            foreach (NetworkInterface adapter in adapters)
-            {
-                IPInterfaceProperties properties = adapter.GetIPProperties();
-                Console.WriteLine(adapter.Description);
-                Console.WriteLine("  DNS suffix .............................. : {0}",
-                                  properties.DnsSuffix);
-                Console.WriteLine("  DNS enabled ............................. : {0}",
-                                  properties.IsDnsEnabled);
-                Console.WriteLine("  Dynamically configured DNS .............. : {0}",
-                                  properties.IsDynamicDnsEnabled);
+            var networkIdProvider = new DefaultNetworkIdProvider();
 
-                Console.WriteLine("  Network Type: ........................... : {0}", adapter.NetworkInterfaceType);
-                Console.WriteLine("  Network Address: ........................ : {0}",
-                                  adapter.GetPhysicalAddress());
-            }
-            Console.WriteLine();
+            byte[] networkId = networkIdProvider.NetworkId;
+
+            Assert.IsNotNull(networkId);
+            Assert.AreEqual(6, networkId.Length);
         }
     }
 }
