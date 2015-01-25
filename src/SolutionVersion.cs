fe7ebe3a059ca45bdb46f2316c9f446a302f343c
Chris Patterson <chris@phatboyg.com>
2015-01-24 18:46:30 -0800

    Strong nameage

--- src/SolutionVersion.cs
+++ src/SolutionVersion.cs
@@ -1,12 +1,12 @@
 using System;
 using System.Reflection;
 using System.Runtime.InteropServices;
-
 [assembly: AssemblyDescription("NewId is an ordered 128-bit unique identifier generator using the Flake algorithm.")]
 [assembly: AssemblyProduct("NewId")]
 [assembly: AssemblyCopyright("Copyright 2015 Chris Patterson, All rights reserved.")]
-[assembly: AssemblyVersion("2.1.1")]
-[assembly: AssemblyFileVersion("2.1.1")]
-[assembly: AssemblyInformationalVersion("2.1.1.7e3008")]
-[assembly: ComVisible(false)]
-[assembly: CLSCompliant(true)]
+[assembly: AssemblyVersion("2.1.2")]
+[assembly: AssemblyFileVersion("2.1.2")]
+
+[assembly: AssemblyInformationalVersion("2.1.2.576827")]
+[assembly: ComVisibleAttribute(false)]
+[assembly: CLSCompliantAttribute(true)]
