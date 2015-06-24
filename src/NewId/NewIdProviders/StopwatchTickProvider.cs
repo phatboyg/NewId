0b659b2165e5a1e64901d1a392f3341dabd29718
RubenAguilar <Ruben.Aguilar@mckesson.com>
2015-06-24 15:59:30 -0700

    Fix StopwatchTickProvider from mixing Timespan ticks and Stopwatch ticks, providing maximum allowable precision

--- src/NewId/NewIdProviders/StopwatchTickProvider.cs
+++ src/NewId/NewIdProviders/StopwatchTickProvider.cs
@@ -7,6 +7,7 @@ namespace MassTransit.NewIdProviders
     public class StopwatchTickProvider :
         ITickProvider
     {
+
         readonly Stopwatch _stopwatch;
         DateTime _start;
 
@@ -16,9 +17,33 @@ namespace MassTransit.NewIdProviders
             _stopwatch = Stopwatch.StartNew();
         }
 
+        private static readonly double StopwatchTickFrequency;
+
+        static StopwatchTickProvider()
+        {
+            if (Stopwatch.IsHighResolution)
+            {
+                StopwatchTickFrequency = TimeSpan.TicksPerSecond / (double)Stopwatch.Frequency;
+            }
+            else
+            {
+                StopwatchTickFrequency = 1.0;
+            }
+        }
+
         public long Ticks
         {
-            get { return _start.AddTicks(_stopwatch.ElapsedTicks).Ticks; }
+            get { return _start.AddTicks(GetStopwatchTicksAsTimespanTicks()).Ticks; }
+        }
+
+        // Stopwatch and Timespan ticks are not the same
+        long GetStopwatchTicksAsTimespanTicks()
+        {
+            long rawTicks = _stopwatch.ElapsedTicks;
+            if (Stopwatch.IsHighResolution)
+                return (long)(rawTicks * StopwatchTickFrequency);
+            else
+                return rawTicks;
         }
     }
 }
