a4de0c11b1a376da9e850da3ce0aafa49dff69d9
Chris Patterson <chris@phatboyg.com>
2015-06-24 19:57:22 -0700

    Fixed test, and provider for stopwatch to get correct timestamp

--- src/NewId.Tests/Provider_Specs.cs
+++ src/NewId.Tests/Provider_Specs.cs
@@ -1,25 +1,49 @@
-using System;
-using System.Threading;
-using MassTransit.NewIdProviders;
-using NUnit.Framework;
-
 namespace MassTransit.NewIdTests
 {
+    using System;
+    using System.Threading;
+    using NewIdProviders;
+    using NUnit.Framework;
+
+
     [TestFixture]
     public class StopwatchTickProvider_Specs
     {
+        [Test, Explicit]
+        public void Should_keep_accurate_time()
+        {
+            TimeSpan timeDelta = TimeSpan.FromSeconds(3);
+
+            var timestamp = DateTime.UtcNow;
+            var provider = new StopwatchTickProvider();
+            long start = provider.Ticks;
+            Thread.Sleep(timeDelta);
+            long stop = provider.Ticks;
+
+            var startTime = new DateTime(start);
+            Console.WriteLine("Start time: {0}, Original: {1}", startTime, timestamp);
+
+
+            long deltaTicks = Math.Abs(stop - start);
+            // 0.01% acceptable delta
+            var acceptableDelta = (long)(timeDelta.Ticks);
+
+            Assert.Less(deltaTicks, acceptableDelta);
+        }
+
         [Test, Explicit]
         public void Should_not_lag_time()
         {
             TimeSpan timeDelta = TimeSpan.FromMinutes(1);
 
-            StopwatchTickProvider startProvider = new StopwatchTickProvider();
+            var startProvider = new StopwatchTickProvider();
             Thread.Sleep(timeDelta);
-            StopwatchTickProvider endProvider = new StopwatchTickProvider();
+            var endProvider = new StopwatchTickProvider();
+
 
             long deltaTicks = Math.Abs(endProvider.Ticks - startProvider.Ticks);
             // 0.01% acceptable delta
-            long acceptableDelta = (long)(timeDelta.Ticks * 0.0001);
+            var acceptableDelta = (long)(timeDelta.Ticks * 0.0001);
 
             Assert.Less(deltaTicks, acceptableDelta);
         }
--- src/NewId/NewIdProviders/StopwatchTickProvider.cs
+++ src/NewId/NewIdProviders/StopwatchTickProvider.cs
@@ -7,7 +7,6 @@ namespace MassTransit.NewIdProviders
     public class StopwatchTickProvider :
         ITickProvider
     {
-
         readonly Stopwatch _stopwatch;
         DateTime _start;
 
@@ -17,33 +16,9 @@ namespace MassTransit.NewIdProviders
             _stopwatch = Stopwatch.StartNew();
         }
 
-        private static readonly double StopwatchTickFrequency;
-
-        static StopwatchTickProvider()
-        {
-            if (Stopwatch.IsHighResolution)
-            {
-                StopwatchTickFrequency = TimeSpan.TicksPerSecond / (double)Stopwatch.Frequency;
-            }
-            else
-            {
-                StopwatchTickFrequency = 1.0;
-            }
-        }
-
         public long Ticks
         {
-            get { return _start.AddTicks(GetStopwatchTicksAsTimespanTicks()).Ticks; }
-        }
-
-        // Stopwatch and Timespan ticks are not the same
-        long GetStopwatchTicksAsTimespanTicks()
-        {
-            long rawTicks = _stopwatch.ElapsedTicks;
-            if (Stopwatch.IsHighResolution)
-                return (long)(rawTicks * StopwatchTickFrequency);
-            else
-                return rawTicks;
+            get { return (_start.AddTicks(_stopwatch.Elapsed.Ticks)).Ticks; }
         }
     }
 }
