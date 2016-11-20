c561633370eb220507bdfdec1464f0a029a0a660
Chris Patterson <chris@phatboyg.com>
2016-11-20 07:24:31 -0800

    Removed extra dependencies to avoid future build complexity

--- src/NewId.Tests/Generator_Specs.cs
+++ src/NewId.Tests/Generator_Specs.cs
@@ -1,96 +1,18 @@
 ﻿namespace MassTransit.NewIdTests
 {
-    using Moq;
-    using NUnit.Framework;
     using System;
     using System.Diagnostics;
+    using NUnit.Framework;
 
     [TestFixture]
     public class When_generating_id
     {
-        Mock<ITickProvider> _tickProviderMock;
-        Mock<IWorkerIdProvider> _workerIdProviderMock;
-        Mock<IProcessIdProvider> _processIdProviderMock;
-        DateTime _start;
-        Stopwatch _stopwatch;
-
-        [SetUp]
-        public void Init()
-        {
-            _tickProviderMock = new Mock<ITickProvider>();
-            _workerIdProviderMock = new Mock<IWorkerIdProvider>();
-            _processIdProviderMock = new Mock<IProcessIdProvider>();
-            _start = DateTime.UtcNow;
-            _stopwatch = Stopwatch.StartNew();
-        }
-
-        private long GetTicks()
-        {
-            return (_start.AddTicks(_stopwatch.Elapsed.Ticks)).Ticks;
-        }
-
-        [Test]
-        public void Should_not_match_when_processor_id_provided()
-        {
-            // Arrange
-            var ticks = GetTicks();
-            var networkPhysicalAddress = BitConverter.GetBytes(1234567890L);
-            var processorId = BitConverter.GetBytes(10);
-
-            _tickProviderMock.SetupGet(x => x.Ticks).Returns(ticks);
-            _workerIdProviderMock.Setup(x => x.GetWorkerId(0)).Returns(networkPhysicalAddress);
-            _processIdProviderMock.Setup(x => x.GetProcessId()).Returns(processorId);
-
-            var generator1 = new NewIdGenerator(_tickProviderMock.Object, _workerIdProviderMock.Object);
-            var generator2 = new NewIdGenerator(_tickProviderMock.Object, _workerIdProviderMock.Object, _processIdProviderMock.Object);
-
-            // Act
-            var id1 = generator1.Next();
-            var id2 = generator2.Next();
-
-            // Assert
-            Assert.AreNotEqual(id1, id2);
-            _processIdProviderMock.Verify(x => x.GetProcessId(), Times.Once());
-        }
-
-        [Test]
-        public void Should_not_match_when_generated_from_two_processes()
-        {
-            // Arrange
-            var ticks = GetTicks();
-            var networkPhysicalAddress = BitConverter.GetBytes(1234567890L);
-            var processorId = 10;
-
-            _tickProviderMock.SetupGet(x => x.Ticks).Returns(ticks);
-            _workerIdProviderMock.Setup(x => x.GetWorkerId(0)).Returns(networkPhysicalAddress);
-            _processIdProviderMock.Setup(x => x.GetProcessId()).Returns(() => BitConverter.GetBytes(processorId++));
-
-            var generator1 = new NewIdGenerator(_tickProviderMock.Object, _workerIdProviderMock.Object, _processIdProviderMock.Object);
-            var generator2 = new NewIdGenerator(_tickProviderMock.Object, _workerIdProviderMock.Object, _processIdProviderMock.Object);
-
-            // Act
-            var id1 = generator1.Next();
-            var id2 = generator2.Next();
-
-            // Assert
-            Assert.AreNotEqual(id1, id2);
-            _processIdProviderMock.Verify(x => x.GetProcessId(), Times.Exactly(2));
-        }
-
         [Test]
         public void Should_match_when_all_providers_equal()
         {
             // Arrange
-            var ticks = GetTicks();
-            var networkPhysicalAddress = BitConverter.GetBytes(1234567890L);
-            var processorId = BitConverter.GetBytes(10);
-
-            _tickProviderMock.SetupGet(x => x.Ticks).Returns(ticks);
-            _workerIdProviderMock.Setup(x => x.GetWorkerId(0)).Returns(networkPhysicalAddress);
-            _processIdProviderMock.Setup(x => x.GetProcessId()).Returns(processorId);
-
-            var generator1 = new NewIdGenerator(_tickProviderMock.Object, _workerIdProviderMock.Object, _processIdProviderMock.Object);
-            var generator2 = new NewIdGenerator(_tickProviderMock.Object, _workerIdProviderMock.Object, _processIdProviderMock.Object);
+            var generator1 = new NewIdGenerator(_tickProvider, _workerIdProvider, _processIdProvider);
+            var generator2 = new NewIdGenerator(_tickProvider, _workerIdProvider, _processIdProvider);
 
             // Act
             var id1 = generator1.Next();
@@ -98,6 +20,103 @@
 
             // Assert
             Assert.AreEqual(id1, id2);
+        }
+
+        [Test]
+        public void Should_not_match_when_generated_from_two_processes()
+        {
+            // Arrange
+            var generator1 = new NewIdGenerator(_tickProvider, _workerIdProvider, _processIdProvider);
+
+            _processIdProvider = new MockProcessIdProvider(BitConverter.GetBytes(11));
+            var generator2 = new NewIdGenerator(_tickProvider, _workerIdProvider, _processIdProvider);
+
+            // Act
+            var id1 = generator1.Next();
+            var id2 = generator2.Next();
+
+            // Assert
+            Assert.AreNotEqual(id1, id2);
+        }
+
+        [Test]
+        public void Should_not_match_when_processor_id_provided()
+        {
+            // Arrange
+            var generator1 = new NewIdGenerator(_tickProvider, _workerIdProvider);
+            var generator2 = new NewIdGenerator(_tickProvider, _workerIdProvider, _processIdProvider);
+
+            // Act
+            var id1 = generator1.Next();
+            var id2 = generator2.Next();
+
+            // Assert
+            Assert.AreNotEqual(id1, id2);
+        }
+
+        [SetUp]
+        public void Init()
+        {
+            _start = DateTime.UtcNow;
+            _stopwatch = Stopwatch.StartNew();
+
+            _tickProvider = new MockTickProvider(GetTicks());
+            _workerIdProvider = new MockNetworkProvider(BitConverter.GetBytes(1234567890L));
+            _processIdProvider = new MockProcessIdProvider(BitConverter.GetBytes(10));
+        }
+
+        ITickProvider _tickProvider;
+        IWorkerIdProvider _workerIdProvider;
+        IProcessIdProvider _processIdProvider;
+        DateTime _start;
+        Stopwatch _stopwatch;
+
+        long GetTicks()
+        {
+            return _start.AddTicks(_stopwatch.Elapsed.Ticks).Ticks;
+        }
+
+        class MockTickProvider :
+            ITickProvider
+        {
+            public MockTickProvider(long ticks)
+            {
+                Ticks = ticks;
+            }
+
+            public long Ticks { get; }
+        }
+
+        class MockNetworkProvider :
+            IWorkerIdProvider
+        {
+            readonly byte[] _workerId;
+
+            public MockNetworkProvider(byte[] workerId)
+            {
+                _workerId = workerId;
+            }
+
+            public byte[] GetWorkerId(int index)
+            {
+                return _workerId;
+            }
+        }
+
+        class MockProcessIdProvider :
+            IProcessIdProvider
+        {
+            readonly byte[] _processId;
+
+            public MockProcessIdProvider(byte[] processId)
+            {
+                _processId = processId;
+            }
+
+            public byte[] GetProcessId()
+            {
+                return _processId;
+            }
         }
     }
 }
