03f4ed3375fa9e1088268fd22630fe39012ac994
Timothy Makkison <timmakkison@gmail.com>
2023-01-04 10:41:40 +0000

    Vectorize NextGuid & NextSequentialGuid, minor code changes.

--- src/NewId/NewId.cs
+++ src/NewId/NewId.cs
@@ -11,8 +11,9 @@ namespace MassTransit
     using System.Runtime.Intrinsics;
     using System.Runtime.InteropServices;
     using System.Diagnostics;
-    using System.Buffers.Binary;
 #endif
+
+
     /// <summary>
     /// A NewId is a type that fits into the same space as a Guid/Uuid/unique identifier,
     /// but is guaranteed to be both unique and ordered, assuming it is generated using
@@ -369,7 +370,7 @@ namespace MassTransit
 
         public override bool Equals(object obj)
         {
-            if (ReferenceEquals(null, obj))
+            if (obj is null)
                 return false;
             if (obj.GetType() != typeof(NewId))
                 return false;
